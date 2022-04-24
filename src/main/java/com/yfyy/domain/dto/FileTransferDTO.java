b1487ee7d74fde5f4764b7c4bf22710047f86b9e
张磊 <vip.p@live.com>
2022-04-24 15:12:46 +0800

    maven version 抽取

---
+++ src/main/java/com/yfyy/domain/dto/FileTransferDTO.java
@@ -0,0 +1,15 @@
+package com.yfyy.domain.dto;
+
+import lombok.Data;
+
+/**
+ * @Author: zhang lei
+ * @Date: 2022/4/24 13:41
+ */
+@Data
+public class FileTransferDTO {
+
+    private String nowPath;
+
+    private String tarPath;
+}
