80b665c01d8fd5ce1dbc336f9e607ae56ac0458e
张磊 <zhangleibq@yonyou.com>
2022-06-20 13:27:18 +0800

    加密

--- src/main/java/com/yfyy/learn/designmode/multition/Emperor.java
+++
@@ -1,9 +0,0 @@
-package com.yfyy.learn.designmode.multition;
-
-/**
- * @Author: zhanglei
- * @DateTime: 2022/6/6 17:23
- */
-public class Emperor {
-    private static int maxNumOfEmperor = 2;
-}
--- src/test/java/com/yfyy/ApplicationTest.java
+++ src/test/java/com/yfyy/ApplicationTest.java
@@ -1,8 +1,12 @@
 package com.yfyy;
 
 import cn.hutool.core.bean.BeanUtil;
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -16,10 +20,21 @@ import java.util.List;
 @RunWith(SpringRunner.class)
 public class ApplicationTest {
 
+    private static final Logger log = LoggerFactory.getLogger(ApplicationTest.class);
+
+    @Autowired
+    private StringEncryptor stringEncryptor;
+
     @Test
     public void test() {
         List<Integer> list = null;
         List<Integer> integers = BeanUtil.copyToList(list, Integer.class);
         System.out.println(integers.toString());
+    }
+
+    @Test
+    public void testPwd() {
+        log.info(stringEncryptor.encrypt("xx"));
+        log.info(stringEncryptor.decrypt("xx"));
     }
 }
